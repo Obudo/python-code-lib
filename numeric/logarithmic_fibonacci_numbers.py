ed87422b722327e3ea561c3a9cbccea6e91a34f7
Luca Pasquali <luca.pasquali.91@gmail.com>
2022-02-20 09:12:55 +0100

    Improved logfib code
    Added comments, generalized to NxN matrix instead of 2x2 only

--- numeric/logarithmic_fibonacci_numbers.py
+++ numeric/logarithmic_fibonacci_numbers.py
@@ -1,4 +1,8 @@
 def mat_prod(mat1, mat2):
+    '''
+    Compute product of two matrices the unga bunga way.
+    Assumes that matrices are compatible
+    '''
     result = [[0 for _ in mat2[0]] for _ in mat1]
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
@@ -8,7 +12,11 @@ def mat_prod(mat1, mat2):
     return result
 
 def matrix_pow(mat, n):
-    retval = [[1, 0], [0, 1]]
+    '''
+    Computes power of a square matrix using mat_prod
+    '''
+    m = len(mat)
+    retval = [[1 if j == i else 0 for j in range(m)] for i in range(m)]
     i = 1
     while i <= n:
         if i & n:
@@ -18,6 +26,10 @@ def matrix_pow(mat, n):
     return retval
 
 def get_fibonacci_number(n):
+    '''
+    Computes the nth fibonacci number in logarithmic time using matrix exponentiation.
+    Can be reused for problems that require quick exponentiation eg. https://leetcode.com/problems/knight-dialer/
+    '''
     initial_matrix = [[1, 1], [1, 0]]
     nth_power_matrix = matrix_pow(initial_matrix, n)
     return nth_power_matrix[1][0]
